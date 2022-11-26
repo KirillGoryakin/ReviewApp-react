6b867083dc5ad39098be057a29ae6d3d6dc85f20
KirillGoryakin <kirillgoryakin.dev@gmail.com>
2022-11-26 18:09:46 +0500

    404 page layout

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,8 +1,34 @@
+import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      Not found 404
+    <div style={{ textAlign: 'center' }}>
+      <Typography
+        fontSize='8rem'
+        fontWeight={700}
+        color='text.secondary'
+      >
+        404
+      </Typography>
+      <Typography
+        fontSize='4rem'
+        fontWeight={700}
+        color='text.secondary'
+      >
+        Page not found
+      </Typography>
+      <Link to='/' replace>
+        <Button
+          variant='outlined'
+          sx={{
+            mt: 6,
+            fontSize: '1.5rem'
+          }}
+        >
+          Home Page
+        </Button>
+      </Link>
     </div>
   )
 }
